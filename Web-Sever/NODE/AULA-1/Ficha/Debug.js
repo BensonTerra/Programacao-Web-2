082f015ba195cb76ed99637fae93008ed642df05
BensonTerra <benson.terra@hotmail.com>
2024-03-01 18:17:54 +0000

    atualizar

---
+++ Web-Sever/NODE/AULA-1/Ficha/Debug.js
@@ -0,0 +1,11 @@
+const {getRandomColor,getAllColors} = require('./EX1.js'); 
+getRandomColor();getAllColors(); 
+
+//const {} = require('./EX2.js');
+
+
+//const {} = require('./EX3.js');
+
+
+//const {} = require('./EX4.js');
+
---
+++ Web-Sever/NODE/AULA-1/Ficha/EX1.js
@@ -0,0 +1,27 @@
+//console.log(process.argv.slice(2))
+
+const allColors = 
+[
+  { name: 'brightred', code: '#E74C3C' },
+  { name: 'soothingpurple', code: '#9B59B6' },
+  { name: 'skyblue', code: '#5DADE2' },
+  { name: 'leafygreen', code: '#48C9B0' },
+  { name: 'sunkissedyellow', code: '#F4D03F' },
+  { name: 'groovygray', code: '#D7DBDD' }
+];
+
+function getRandomColor() 
+{
+  let color = allColors[Math.floor(Math.random()*allColors.length)]; //console.log(color);
+  console.log('getRandomColor | ' + "Color: " + color.name + ' | Hex:' + color.code)
+}
+
+const getAllColors = () => 
+{
+  allColors.forEach( item => console.log('getAllColor | ' + "Color: " + item.name + ' | Hex:' + item.code))
+}
+
+module.exports = 
+{
+  getRandomColor,getAllColors
+};
