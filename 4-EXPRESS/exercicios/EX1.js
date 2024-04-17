5881409ee1271239b6b030d3d58bcbb63ef2c438
BensonTerra <benson.terra@hotmail.com>
2024-04-17 10:25:36 +0100

    sa

---
+++ 4-EXPRESS/exercicios/EX1.js
@@ -0,0 +1,13 @@
+// import Express
+const express = require('express');
+const app = express();
+const hostname = '127.0.0.1';
+const port = process.env.PORT || 3000;
+// sets the server response to a GET request on URL “/”
+app.get('/', (req, res) => {
+res.send('<html><body><h1>Hello World</h1></body></html>');
+})
+// server creation and listening for any incoming requests
+app.listen(port, hostname, (error) => {
+console.log(`App listening at http://${hostname}:${port}/`)
+})
