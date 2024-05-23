e68fb2f69070da55be9a8e5f3a6f45cd3c4c0f44
BensonTerra <benson.terra@hotmail.com>
2024-05-23 16:51:34 +0100

    7 - Moongoose

---
+++ 7 - Mongoose/Tutorials API (Mongoose)/controllers/comments.controller.js
@@ -0,0 +1,53 @@
+const db = require("../models/index.js");
+const Comment = db.comments;
+const Tutorial = db.tutorials;
+
+// Create and Save a new Comment
+exports.create = async (req, res) => {
+    try {
+        const comment = new Comment({
+            text: req.body.text
+        });
+        let data = await comment.save();
+
+        const tutorial = await Tutorial.findByIdAndUpdate(
+            req.params.idT,
+            { $push: { comments: [data._id] } }
+        ).exec();
+        // no data returned means there is no tutorial in DB with that given ID 
+        if (tutorial === null) {
+            await Comment.findByIdAndRemove(data._id).exec();// remove the previously created comment
+            return res.status(404).json({
+                success: false, msg: `Cannot find any tutorial with ID ${req.params.idT}.`
+            });
+        }
+
+        res.status(201).json({ success: true, msg: "New Comment created.", URL: "/tutorials/" + req.params.idT + "/comments/" + data._id });
+    }
+    catch (err) {
+        if (err.name === "CastError") {
+            return res.status(400).json({
+                success: false, msg: "ID parameter is not a valid ObjectId (it must be must be a single String of 12 bytes or a string of 24 hex characters)"
+            });
+        }
+        res.status(500).json({
+            success: false, msg: `Some error occurred while creating the Comment.`
+        });
+    }
+};
+
+
+// Delete a Comment with the specified id in the request
+exports.delete = async (req, res) => {
+    try {
+        // TODO
+        return res.status(200).json({
+            success: true, msg: `Comment id=${req.params.idC} was deleted successfully.`
+        });
+
+    } catch (err) {
+        res.status(500).json({
+            message: `Error deleting Comment with id=${req.params.commentID}.`
+        });
+    };
+};
---
+++ 7 - Mongoose/Tutorials API (Mongoose)/routes/comments.routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const commentController = require("../controllers/comments.controller");
+
+// set 'mergeParams: true' on the router to access params from the parent router (like idT req parameter)​
+let router = express.Router({mergeParams: true});
+
+router.route('/')
+    .post( commentController.create); //POST /tutorials/:idT/comments
+
+router.route('/:idC')
+    .delete( commentController.delete); // DELETE /tutorials/:idT/comments/:idC
+
+router.all('*', function (req, res) {
+    //send an predefined error message 
+    res.status(404).json({ message: 'COMMENTS: what???' });
+})
+
+module.exports = router;
