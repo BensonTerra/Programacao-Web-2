17f4dbcdf6cc818bc5f82ad6672ddb34ded72f56
BensonTerra <benson.terra@hotmail.com>
2025-07-04 15:11:29 +0100

    moongose

--- 9 - Prova/MongoDB/controllers/users.controller.js
+++ 9 - Prova/MongoDB/controllers/users.controller.js
@@ -1,28 +1,30 @@
+const clear = require('clear');
 const db = require('../models/db.js'); // Import the database connection
+
+function escapeRegex(str) {
+    return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
 
 // get all posts from a user 
 let getPostsFromUser = async (req, res, next) => {
-
     try {
+        clear()
         let user = await db.User.findById(req.params.id)
-            .select('-password -__v') // exclude the __v and password fields 
-            .exec()
+            .select('-password -__v')
+            .exec();
 
-        // If not found, return 404
         if (!user) {
             let error = new Error(`Cannot find any USER with ID ${req.params.id}.`);
             error.statusCode = 404;
-            return next(error); // Pass the error to the next middleware
+            return next(error);
         }
 
-        // "lazy loading" posts from the user
         const posts = await db.Post.find({ author: req.params.id })
-            .select('-__v -author') // exclude the author and __v fields
+            .select('-__v -author')
             .exec();
 
-        // map HATEOAS links to each one of the posts
         const postsWithLinks = posts.map(post => ({
-            ...post.toJSON(), // spread operator to copy all properties of the post object
+            ...post.toJSON(),
             links: [
                 { rel: "self", href: `/posts/${post._id}`, method: "GET" },
                 { rel: "modify", href: `/posts/${post._id}`, method: "PUT" },
@@ -30,18 +32,35 @@ let getPostsFromUser = async (req, res, next) => {
             ]
         }));
 
-        user = user.toJSON(); // convert the user to a plain object
-        user.posts = postsWithLinks; // add posts to the user object
+        user = user.toJSON();
+        user.posts = postsWithLinks;
 
         return res.status(200).json(user);
-    }
-    catch (err) {
+    } catch (err) {
         next(err);
     }
-}
+};
 
+let getAllUsers = async (req, res, next) => {
+    try {
+        clear()
+        const { username, role } = req.query;
+        const query = {};
 
+        if (username) query.username = { $regex: escapeRegex(username), $options: 'i' };
+        if (role)     query.role     = { $regex: escapeRegex(role),     $options: 'i' };
+
+        console.log("Query MongoDB:", query); // debug
+
+        const users = await db.User.find(query).select('-password -__v');
+
+        return res.status(200).json(users);
+    } catch (err) {
+        next(err);
+    }
+};
 
 module.exports = {
-    getPostsFromUser
-}
+    getPostsFromUser,
+    getAllUsers
+};
--- 9 - Prova/MongoDB/routes/users.routes.js
+++ 9 - Prova/MongoDB/routes/users.routes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 // include controller functions
 const usersController = require('../controllers/users.controller.js');
 
+router.get('/', usersController.getAllUsers);
 router.get('/:id/posts', usersController.getPostsFromUser); // get all posts from a user
 
 module.exports = router;
